3af1ba68f46358fd1720d36dbf4e8786e342030d
Zakaria Eysa <zakariaeysa@gmail.com>
2024-08-07 15:50:23 +0300

    App Done With Mvvm architecture

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,14 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final String price;
+  final String name;
+
+  BookEntity(
+      {required this.image,
+      required this.title,
+      required this.authorName,
+      required this.price,
+      required this.name});
+}
