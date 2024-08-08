d2e3b2636931fe96846b45e139aca9ded02f44d9
Zakaria Eysa <zakariaeysa@gmail.com>
2024-08-08 14:20:37 +0300

    edits in DI

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app_project/Features/home/data/data_sources/local_data_source/home_local_data_source_impl.dart';
+import 'package:bookly_app_project/Features/home/data/data_sources/remote_data_source/home_remote_data_source_impl.dart';
 import 'package:bookly_app_project/Features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app_project/core/utils/api_service.dart';
 import 'package:dio/dio.dart';
@@ -7,5 +9,11 @@ final getIt = GetIt.instance;
 
 void setupServiceLocator() {
   getIt.registerSingleton<ApiService>(ApiService(Dio()));
-  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(getIt.get<ApiService>()));
+  getIt.registerSingleton<HomeRemoteDataSourceImpl>(
+      HomeRemoteDataSourceImpl(getIt.get<ApiService>()));
+  getIt.registerSingleton<HomeLocalDataSourceImpl>(HomeLocalDataSourceImpl());
+
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+      homeLocalDataSource: getIt.get<HomeLocalDataSourceImpl>(),
+      homeRemoteDataSource: getIt.get<HomeRemoteDataSourceImpl>()));
 }
